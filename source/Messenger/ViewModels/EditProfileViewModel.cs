548431c3a0ad9c8468a2beca2fd24694dc40b5a8
Khanh <vedakinvn@gmail.com>
2025-03-11 02:05:25 +0700

    Cập nhật tiến độ

--- source/Messenger/ViewModels/EditProfileViewModel.cs
+++ source/Messenger/ViewModels/EditProfileViewModel.cs
@@ -1,12 +1,16 @@
-﻿namespace Messenger.ViewModels
+﻿using System;
+
+namespace Messenger.ViewModels
 {
     public class EditProfileViewModel : BaseViewModel
     {
         public string EditUsername { get; set; }
         public string EditEmail { get; set; }
+        public string EditPhoneNumber { get; set; }
+        public DateTime EditBirthday { get; set; }
+
         public EditProfileViewModel()
         {
-
         }
     }
 }
